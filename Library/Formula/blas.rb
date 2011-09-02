7e1761c22109f2953d71798d50bec1d9543dc99a
Rui Pereira <rui.pereira@gmail.com>
2011-09-02 16:25:47 +0200

    Deparallelize.

--- Library/Formula/blas.rb
+++ Library/Formula/blas.rb
@@ -4,9 +4,10 @@ class Blas < Formula
   url 'http://www.netlib.org/blas/blas.tgz'
   homepage 'http://www.netlib.org/blas'
   md5 '5e99e975f7a1e3ea6abcad7c6e7e42e6'
-  version '19.04.2011'
+  version '2011.04.19'
 
   def install
+    ENV.deparallelize
     ENV.fortran
     system "make"
     lib.install 'blas_LINUX.a' =>'libblas.a'
