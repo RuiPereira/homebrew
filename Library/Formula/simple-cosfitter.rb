bb88e5569ef9609e5c79307b77bfc058c83d3e5f
Rui Pereira <rui.pereira@gmail.com>
2012-03-04 12:15:21 +0100

    simple-cosfitter 1.6.11
    simple-cosfitter is an implementation of a fairly simple-minded
    luminosity distance fitter, intended for use with supernova data.

---
+++ Library/Formula/simple-cosfitter.rb
@@ -0,0 +1,32 @@
+require 'formula'
+
+class SimpleCosfitter < Formula
+  homepage 'http://qold.astro.utoronto.ca/conley/simple_cosfitter'
+  url 'http://qold.astro.utoronto.ca/conley/simple_cosfitter/simple_cosfitter-1.6.11.tar.gz'
+  md5 '5c079c7a37b60cffb00796e5b317eeb9'
+
+  depends_on 'cfitsio'
+  depends_on 'gsl'
+  depends_on 'doxygen' if ARGV.include? '--with-docs'
+
+  def options
+    [['--with-docs', 'Build documentation']]
+  end
+
+  def install
+    system "./configure", "--prefix=#{prefix}", "--enable-cfitswrite", "--with-cfitsiolib=#{HOMEBREW_PREFIX}"
+    system "make all"
+    system "make docs" if ARGV.include? '--with-docs'
+    system "make install"
+    prefix.install Dir['sample']
+    doc.install Dir['doc/*'] if ARGV.include? '--with-docs'
+  end
+
+  def test
+    mktemp do
+      cp Dir["#{prefix}/sample/Kowalski*"], '.'
+      system "runfitter Kowalski_wom_param.txt"
+      system "convert_to_fits Kowalski_wom_param.txt Kowalski_wom_output.fits"
+    end
+  end
+end
