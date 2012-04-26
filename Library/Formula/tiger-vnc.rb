fc569fe4f7ab28de2a43b13e131d8f467d3a139b
Rui Pereira <rui.pereira@gmail.com>
2012-04-26 10:56:26 +0200

    Update tiger-vnc to 1.2.0.
    Now using cmake, libjpeg-turbo and (optionally) gnutls.

--- Library/Formula/tiger-vnc.rb
+++ Library/Formula/tiger-vnc.rb
@@ -2,18 +2,23 @@ require 'formula'
 
 class TigerVnc < Formula
   homepage 'http://tigervnc.org/'
-  url 'http://downloads.sourceforge.net/project/tigervnc/tigervnc/1.0.1/tigervnc-1.0.1.tar.gz'
-  md5 'daca3eb16afe65253b766eccb8bfdbc5'
+  url 'http://downloads.sourceforge.net/project/tigervnc/tigervnc/1.2.0/tigervnc-1.2.0.tar.gz'
+  md5 '3a5755b4ed600a81c3a17976c6f8420d'
+
+  depends_on 'cmake' => :build
+  depends_on 'jpeg-turbo'
+  depends_on 'gnutls' => :recommended
 
   def install
-    cd "unix" do
-      system "./configure", "--disable-debug", "--disable-dependency-tracking",
-                            "--prefix=#{prefix}"
-      system "make install"
-    end
-  end
-
-  def caveats
-    "NOTE: This is X11 software."
+    ENV.x11
+    # also install unix subdir
+    inreplace 'CMakeLists.txt', 'add_subdirectory(vncviewer)', "add_subdirectory(vncviewer)\nadd_subdirectory(unix)"
+    jpegdir = `brew --prefix jpeg-turbo`.chomp
+    jpeglib = Dir["#{jpegdir}/lib/libjpeg.a"][0]
+    system "cmake -G \"Unix Makefiles\" #{std_cmake_parameters} -DENABLE_NLS:BOOL=OFF -DJPEG_INCLUDE_DIR=#{jpegdir}/include -DJPEG_LIBRARY=#{jpeglib}"
+    system "make install"
+    # move man pages to proper path
+    man.mkpath
+    mv Dir["#{prefix}/man/*"], man
   end
 end
