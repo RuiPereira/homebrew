27cad3120ed54f218e34d98516286e3ef7308b81
Rui Pereira <rui.pereira@gmail.com>
2013-01-06 15:32:47 +0100

    Revert some tests.

--- Library/Formula/ghostscript.rb
+++ Library/Formula/ghostscript.rb
@@ -24,7 +24,6 @@ class Ghostscript < Formula
   depends_on 'jbig2dec'
   depends_on 'little-cms2'
   depends_on :libpng
-#  depends_on :x11 #=> '2.7.2'
 
   # Fix dylib names, per installation instructions
   def patches
@@ -55,6 +54,7 @@ class Ghostscript < Formula
         --disable-compile-inits
         --disable-gtk
         --with-system-libtiff
+        --without-x
       ]
 
       if build.head?
